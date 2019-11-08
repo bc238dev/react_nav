0961a26961b9afbd122bd4ffd82735b1be8de0f5
bc238dev <46238915+bc238dev@users.noreply.github.com>
2019-11-07 16:40:43 -0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react_nav
-Created with CodeSandbox
---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import { render } from "react-dom"
+
+import "./styles.css"
+
+const Nav = ({ children }) => <div className="nav">{children}</div>
+
+function App() {
+  return (
+    <div className="App">
+      <Nav>Hello</Nav>
+    </div>
+  )
+}
+
+const rootElement = document.getElementById("root")
+render(<App />, rootElement)
